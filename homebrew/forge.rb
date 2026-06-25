a097902c635aeb7d68ff7e08aabf116eb92719bb
Floris <floris.voskamp@outlook.com>
2026-06-25 11:02:59 +0200

    chore: fill homebrew sha256 for v0.4.2 (#216)

--- homebrew/forge.rb
+++ homebrew/forge.rb
@@ -15,18 +15,18 @@ class Forge < Formula
   on_macos do
     on_arm do
       url "https://github.com/florisvoskamp/forge/releases/download/v#{version}/forge-aarch64-apple-darwin.tar.gz"
-      sha256 "0000000000000000000000000000000000000000000000000000000000000000"
+      sha256 "c78fab82b29aa980e5b6376cff3f40e092f3d0aed12812204a1fa6bf153df2d0"
     end
     on_intel do
       url "https://github.com/florisvoskamp/forge/releases/download/v#{version}/forge-x86_64-apple-darwin.tar.gz"
-      sha256 "0000000000000000000000000000000000000000000000000000000000000000"
+      sha256 "8869eefcc9761d4aa5ba54171dff077d5c15d3f1cfd759c60f349ada8a417dc1"
     end
   end
 
   on_linux do
     on_intel do
       url "https://github.com/florisvoskamp/forge/releases/download/v#{version}/forge-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "0000000000000000000000000000000000000000000000000000000000000000"
+      sha256 "47578ef1b3a73908e8278fe5d7c42d68248a5db99c31e5c819412e79d5a9eaa8"
     end
   end
 
