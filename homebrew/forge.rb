c7d53fb4b979d4ade260cc45f62f01453026887e
Floris <floris.voskamp@outlook.com>
2026-07-05 16:17:07 +0200

    chore(brew): update formula to v2.5.3 (#522)
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- homebrew/forge.rb
+++ homebrew/forge.rb
@@ -15,22 +15,22 @@ class Forge < Formula
   on_macos do
     on_arm do
       url "https://github.com/florisvoskamp/forge/releases/download/v#{version}/forge-aarch64-apple-darwin.tar.gz"
-      sha256 "0000000000000000000000000000000000000000000000000000000000000000"
+      sha256 "99f80802b231bbd8823b0b8d86e2949205c0d4188e1650d52e3c2d89cdbc5598"
     end
     on_intel do
       url "https://github.com/florisvoskamp/forge/releases/download/v#{version}/forge-x86_64-apple-darwin.tar.gz"
-      sha256 "0000000000000000000000000000000000000000000000000000000000000000"
+      sha256 "039fde08c9f6f0d17db07fb885e8982a3553b3891176d9cc1aa2e7e493a5cba3"
     end
   end
 
   on_linux do
     on_intel do
       url "https://github.com/florisvoskamp/forge/releases/download/v#{version}/forge-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "0000000000000000000000000000000000000000000000000000000000000000"
+      sha256 "39662a9917b2c422d5c5698954b4129821d466d927e9617b88752e3e4e1f94ed"
     end
     on_arm do
       url "https://github.com/florisvoskamp/forge/releases/download/v#{version}/forge-aarch64-unknown-linux-gnu.tar.gz"
-      sha256 "0000000000000000000000000000000000000000000000000000000000000000"
+      sha256 "3dff50ae6208fb6e2937f9c199c2addf3a7afdc7f8afdfa1b77317980cb6d3ce"
     end
   end
 
